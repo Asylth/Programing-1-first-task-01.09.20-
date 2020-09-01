20347c0e5f4da7dca311229dd860860b4c195520
Asylth <44904158+Asylth@users.noreply.github.com>
2020-09-01 14:11:37 +0200

    Add project files.

---
+++ Programing 1 first task (01.09.20)/Programing 1 first task (01.09.20).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int user_age;
+
+int main()
+{
+	std::cout << "What is your age: \n";
+	//std::getline(std::cin >>, user_age); did not work for some reason
+	std::cin >> user_age;
+	if (user_age > 20)
+	{
+	std::cout << "You are an adult \n";
+	}
+	else
+	{
+	std::cout << "You are a teenager \n";
+	}
+}
